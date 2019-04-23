fd7aec0d9e63bc17d91fe48ff59fc534e7159e8e
Paul2803k <p.scattolin95@gmail.com>
2019-04-23 12:41:51 +0200

    code refractor

--- src/assignment_9/Assignment_9.java
+++ src/assignment_9/Assignment_9.java
@@ -12,6 +12,7 @@ import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 import javafx.scene.shape.Arc;
 import javafx.scene.shape.ArcType;
 import javafx.stage.Stage;
@@ -33,136 +34,54 @@ public class Assignment_9 extends Application {
 
         root.setLeft(vb);
         root.setCenter(diagram);
-
-        TextField textField = new TextField();
-        TextField textField2 = new TextField();
-        TextField textField3 = new TextField();
-        TextField textField4 = new TextField();
-
-        textField.textProperty().addListener(new ChangeListener< String>() {
-            @Override
-            public void changed(
-                    ObservableValue<? extends String> observable,
-                    String oldValue, String newValue) {
-                if (!newValue.matches(" [1 -9]\\ d{0 ,3}")) {
-                    textField.setText(oldValue);
-                }
-            }
-        }
-        );
-        textField2.textProperty().addListener(new ChangeListener< String>() {
-            @Override
-            public void changed(
-                    ObservableValue<? extends String> observable,
-                    String oldValue, String newValue) {
-                if (!newValue.matches(" [1 -9]\\ d{0 ,3}")) {
-                    textField.setText(oldValue);
-                }
-            }
-        }
-        );
-        textField3.textProperty().addListener(new ChangeListener< String>() {
-            @Override
-            public void changed(
-                    ObservableValue<? extends String> observable,
-                    String oldValue, String newValue) {
-                if (!newValue.matches(" [1 -9]\\ d{0 ,3}")) {
-                    textField.setText(oldValue);
-                }
-            }
-        }
-        );
-        textField4.textProperty().addListener(new ChangeListener< String>() {
-            @Override
-            public void changed(
-                    ObservableValue<? extends String> observable,
-                    String oldValue, String newValue) {
-                if (!newValue.matches(" [1 -9]\\ d{0 ,3}")) {
-                    textField.setText(oldValue);
-                }
-            }
-        }
-        );
-
-        IntegerProperty inValue = new SimpleIntegerProperty();
-        IntegerProperty inValue2 = new SimpleIntegerProperty();
-        IntegerProperty inValue3 = new SimpleIntegerProperty();
-        IntegerProperty inValue4 = new SimpleIntegerProperty();
+        
+        final TextField[] t;
+        t = new TextField[4];
+        
+        IntegerProperty[] i;
+        i = new SimpleIntegerProperty[4];
+        
+        DoubleProperty[] d;
+        d = new SimpleDoubleProperty[4];
+        
+        Arc[] a;
+        a = new Arc[4];
+        
         IntegerProperty sum = new SimpleIntegerProperty();
+        
+        for(int j = 0 ; j < 4; j++){
+            
+            t[j] = new TextField();
+            i[j] = new SimpleIntegerProperty();
+            
+            vb.getChildren().add(t[j]);
+            t[j].textProperty().bindBidirectional(i[j], new NumberStringConverter());  
+        }
+        
+        sum.bind(i[0].add(i[1]).add(i[2]).add(i[3])); 
+        Color[] c = {Color.RED, Color.BLUE, Color.GREEN, Color.ORANGE};
+        
+        for(int j = 0 ; j < 4; j++){
+            
+            a[j] = new Arc();
+            d[j] = new SimpleDoubleProperty();
+            
+            d[j].bind(i[j].add(0f).divide(sum));
+            a[j].setType(ArcType.ROUND);
+            a[j].setFill(c[j]);
+            a[j].centerXProperty().bind(diagram.widthProperty().divide(2.0f));
+            a[j].centerYProperty().bind(diagram.heightProperty().divide(2.0f));
+            a[j].lengthProperty().bind(d[j].multiply(360.0f));
+            a[j].radiusXProperty().bind(diagram.widthProperty().multiply(0.45));
+            a[j].radiusYProperty().bind(diagram.widthProperty().multiply(0.45));
 
-        sum.bind(inValue.add(inValue2).add(inValue3).add(inValue4));
-
-        DoubleProperty outValue = new SimpleDoubleProperty();
-        DoubleProperty outValue2 = new SimpleDoubleProperty();
-        DoubleProperty outValue3 = new SimpleDoubleProperty();
-        DoubleProperty outValue4 = new SimpleDoubleProperty();
-
-        textField.textProperty().bindBidirectional(inValue, new NumberStringConverter());
-        textField2.textProperty().bindBidirectional(inValue2, new NumberStringConverter());
-        textField3.textProperty().bindBidirectional(inValue3, new NumberStringConverter());
-        textField4.textProperty().bindBidirectional(inValue4, new NumberStringConverter());
-
-        outValue.bind(inValue.add(0f).divide(sum));
-        outValue2.bind(inValue2.add(0f).divide(sum));
-        outValue3.bind(inValue3.add(0f).divide(sum));
-        outValue4.bind(inValue4.add(0f).divide(sum));
-
-        Arc arc = new Arc();
-        arc.setType(ArcType.ROUND);
-
-        arc.setFill(javafx.scene.paint.Color.RED);
-
-        arc.centerXProperty().bind(diagram.widthProperty().divide(2.0f));
-        arc.centerYProperty().bind(diagram.heightProperty().divide(2.0f));
-        arc.lengthProperty().bind(outValue.multiply(360.0f));
-        arc.radiusXProperty().bind(diagram.widthProperty().multiply(0.45));
-        arc.radiusYProperty().bind(diagram.widthProperty().multiply(0.45));
-
-        Arc arc2 = new Arc();
-        arc2.setType(ArcType.ROUND);
-
-        arc2.setFill(javafx.scene.paint.Color.BLUE);
-
-        arc2.centerXProperty().bind(diagram.widthProperty().divide(2.0f));
-        arc2.centerYProperty().bind(diagram.heightProperty().divide(2.0f));
-
-        arc2.startAngleProperty().bind(arc.lengthProperty());
-        arc2.lengthProperty().bind(outValue2.multiply(360.0f));
-
-        arc2.radiusXProperty().bind(diagram.widthProperty().multiply(0.45));
-        arc2.radiusYProperty().bind(diagram.widthProperty().multiply(0.45));
-
-        Arc arc3 = new Arc();
-        arc3.setType(ArcType.ROUND);
-
-        arc3.setFill(javafx.scene.paint.Color.GREEN);
-
-        arc3.centerXProperty().bind(diagram.widthProperty().divide(2.0f));
-        arc3.centerYProperty().bind(diagram.heightProperty().divide(2.0f));
-
-        arc3.startAngleProperty().bind(arc.lengthProperty().add(arc2.lengthProperty()));
-        arc3.lengthProperty().bind(outValue3.multiply(360.0f));
-
-        arc3.radiusXProperty().bind(diagram.widthProperty().multiply(0.45));
-        arc3.radiusYProperty().bind(diagram.widthProperty().multiply(0.45));
-
-        Arc arc4 = new Arc();
-        arc4.setType(ArcType.ROUND);
-
-        arc4.setFill(javafx.scene.paint.Color.ORANGE);
-
-        arc4.centerXProperty().bind(diagram.widthProperty().divide(2.0f));
-        arc4.centerYProperty().bind(diagram.heightProperty().divide(2.0f));
-
-        arc4.startAngleProperty().bind(arc.lengthProperty().add(arc2.lengthProperty()).add(arc3.lengthProperty()));
-        arc4.lengthProperty().bind(outValue4.multiply(360.0f));
-
-        arc4.radiusXProperty().bind(diagram.widthProperty().multiply(0.45));
-        arc4.radiusYProperty().bind(diagram.widthProperty().multiply(0.45));
-
-        diagram.getChildren().addAll(arc, arc2, arc3, arc4);
-        vb.getChildren().addAll(textField, textField2, textField3, textField4);
-
+            diagram.getChildren().add(a[j]);
+        }
+        
+        a[1].startAngleProperty().bind(a[0].lengthProperty());
+        a[2].startAngleProperty().bind(a[0].lengthProperty().add(a[1].lengthProperty()));
+        a[3].startAngleProperty().bind(a[0].lengthProperty().add(a[1].lengthProperty()).add(a[2].lengthProperty()));
+        
         Scene scene = new Scene(root, 600, 600);
 
         primaryStage.setTitle("Assignment 9");
